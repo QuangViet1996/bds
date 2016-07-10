6132e490dc02cb789d663c935ecfceab1361ee3d
TheLocNguyen <pykun6@gmai.com>
2016-07-10 16:18:47 +0700

    a
    a

---
+++ templates/BDS_php/DetailCata_BDS.php
@@ -0,0 +1,83 @@
+<section class="page-title">
+    <div class="auto-container">
+        <div class="content-box">
+            <h1>Details cata</h1>
+            <div class="bread-crumb">
+                <ul class="breadcrumb pull-right"><li><a href="#">Home</a></li><li><a href="#">Archive for January, 2016</a></li></ul>			</div>
+        </div>
+    </div>
+</section>
+<div class="sidebar-page">
+    <div class="container">
+        <div class="row clearfix">
+            <!--Sidebar-->
+
+            <!--Content Side-->	
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <section class="blog-container blog-detail">
+                    <!--Blog Post-->
+                    <div class="blog-post">
+                        <!--Blog Post-->
+                        <div class="blog-post wow fadeInLeft animated" data-wow-delay="0ms" data-wow-duration="1500ms" style="visibility: visible; animation-duration: 1500ms; animation-delay: 0ms; animation-name: fadeInLeft;">
+                            <article class="inner-box">
+                                <figure class="image-box">
+                                    <a href="#"><img width="625" height="301" src="images/blog-image-6.jpg" class="img-responsive wp-post-image" alt="blog-image-6" sizes="(max-width: 625px) 100vw, 625px"></a>
+                                    <div class="post-options">
+                                        <a href="#" class="heart-icon img-circle jolly_like_it" data-id="270"><span class="icon fa fa-heart"></span></a>
+                                    </div>
+                                </figure>
+                                <!--Lower Content-->
+                                <div class="lower-part">
+                                    <div class="post-info">
+                                        Posted On<a href="#"><strong>Jan 13, 2016</strong></a>
+                                    </div>
+                                    <div class="post-title"><h3><a href="#">This is post with big title and image</a></h3></div>
+                                    <div class="post-text">
+                                        <p></p><p>lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown lip printer took a galley of type and scrambled it to make a type specimen book. .</p>
+                                        <blockquote><p>“ There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour lorem ipsum making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words. “</p></blockquote>
+                                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don’t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn’t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words.</p>
+                                        <p></p>
+                                    </div>
+                                    <div class="post-info lower">
+                                        <i class="fa fa-comments"></i> &nbsp; <a href="#"><strong>No Comments</strong> &nbsp; &nbsp; &nbsp;</a> | &nbsp; &nbsp; <a href="#"><i class="fa fa-tag"></i> &nbsp; </a><a href="http://wp1.themexlab.com/wp/dreamland/?cat=33" rel="category">Residential</a>
+                                        <span class="pull-right">
+                                            <div class="share-box">
+                                                <ul class="psocial shares clearfix">
+                                                    <li class="facebook"><span class="st_facebook_large hovicon effect-1 sub-a" st_processed="yes"><span style="text-decoration:none;color:#000000;display:inline-block;cursor:pointer;" class="stButton"><span class="stLarge"></span></span></span></li>
+                                                    <li class="twitter"><span class="st_twitter_large hovicon effect-1 sub-a" st_processed="yes"><span style="text-decoration:none;color:#000000;display:inline-block;cursor:pointer;" class="stButton"><span class="stLarge"></span></span></span></li>
+                                                    <li class="google"><span class="st_googleplus_large hovicon effect-1 sub-a" st_processed="yes"><span style="text-decoration:none;color:#000000;display:inline-block;cursor:pointer;" class="stButton"><span class="stLarge"></span></span></span></li>
+                                                    <li class="pinterest"><span class="st_pinterest_large hovicon effect-1 sub-a" st_processed="yes"><span style="text-decoration:none;color:#000000;display:inline-block;cursor:pointer;" class="stButton"><span class="stLarge"></span></span></span></li>
+
+                                                </ul>
+
+                                                <br>
+                                            </div>
+                                        </span>
+                                    </div>
+                                    <div class="post-tags">
+                                    </div>
+
+                                </div>
+                            </article>
+                            <br>
+                            <!-- Comment Form -->
+
+
+                            <!-- end comments -->
+                        </div>
+
+                    </div>
+                </section> 
+
+                <!--Pagination-->
+
+
+            </div>
+            <!--Content Side-->
+
+            <!--Sidebar-->
+
+            <!--Sidebar-->
+        </div>
+    </div>
+</div>
---
+++ templates/BDS_php/FooterMain_BDS.php
@@ -0,0 +1,40 @@
+<footer class="main-footer">
+
+    <div class="auto-container">
+
+
+
+        <!--Footer Content-->
+
+        <div class="footer-content">
+
+
+            <div class="social-links">
+
+
+                <a title="Facebook" href="#"><span class="fa fa-facebook"></span></a>
+
+
+                <a title="Twitter" href="#"><span class="fa fa-twitter"></span></a>
+
+
+                <a title="Linked in" href="#"><span class="fa fa-linkedin"></span></a>
+
+
+                <a title="Google Plus" href="#"><span class="fa fa-google-plus"></span></a>
+
+
+            </div>
+
+
+
+
+            <div class="copyright">Copryright 2015 by Dreamland | All rights reserved</div>
+
+        </div>
+
+
+
+    </div>
+
+</footer>
