2ac4386891dc719747838b630afb503241401a9c
TheLocNguyen <pykun6@gmai.com>
2016-07-25 10:19:20 +0700

    a
    a

--- src/resources/views/vendor/laravel-authentication-acl/admin/realestates/images-table.blade.php
+++ src/resources/views/vendor/laravel-authentication-acl/admin/realestates/images-table.blade.php
@@ -26,49 +26,52 @@ if (!empty($realestate)) {
 </div>
 
 @endif
-    {{ Form::hidden('is_file', !empty(@$realestate->real_estate_image)?1:0, ['class' => 'is_file']) }}
+{{ Form::hidden('is_file', !empty(@$realestate->real_estate_image)?1:0, ['class' => 'is_file']) }}
 
 @if($arr_images)
-    <table class="table table-hover images-table">
-        <thead>
-            <tr>
-                <th style="width: 5%">STT</th>
-                <th style="width: 25%">Hình</th>
-                <th style="width: 50%">Mô tả</th>
-                <th style="width: 205%">Thao tác</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php $counter = 1; ?>
-            @foreach($arr_images as $index=> $item)
-            <tr class="item_image">
-                    <td><?php echo $counter; $counter++; ?></td>
-                    <td>
-                        <div class="img-thumb">
-                            <img src="{!! url($data['configs']['urlpath'].'/'.$item->name) !!}">
-                            <input type='hidden' name='images_name[]' value="<?php echo $item->name ?>" >
-                        </div>
-                    </td>
-                    <td>
-                        <div class="form-group">
-                            {!! Form::textarea('images_info[]',$item->info, ['class' => 'form-control view-write', 'placeholder' => trans('re.description'), 'rows' => 7, 'cols' => 40]) !!}
-                            <div class='view-read'>
-                                {!! $item->info !!}
-                            </div>
-                        </div>
-                    </td>
-                    <td>
-                        <span href="#" title='down' class='down'><i class="fa fa-sort-desc fa-2x" aria-hidden="true"></i></span>
-                        <span href="#" title='up' class='up'><i class="fa fa-sort-asc fa-2x" aria-hidden="true"></i></span>
-                        <span href="#" title='view' class='view'><i class="fa fa-eye fa-2x"></i></span>
-                        <span href="#" title='edit' class="edit margin-left-5"><i class="fa fa-pencil-square-o fa-2x"></i></span>
-                        <span href="#" title='delete' class="margin-left-5"><i class="fa fa-trash-o delete fa-2x"></i></span>
-
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+<table class="table table-hover images-table">
+    <thead>
+        <tr>
+            <th style="width: 5%">STT</th>
+            <th style="width: 25%">Hình</th>
+            <th style="width: 50%">Mô tả</th>
+            <th style="width: 205%">Thao tác</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php $counter = 1; ?>
+        @foreach($arr_images as $index=> $item)
+        <tr class="item_image">
+            <td><?php echo $counter;
+        $counter++;
+        ?></td>
+            <td>
+                <div class="img-thumb">
+                    <img src="{!! url($data['configs']['urlpath'].'/'.$item->name) !!}">
+                    <input type='hidden' name='images_name[]' value="<?php echo $item->name ?>" >
+                </div>
+            </td>
+            <td>
+                <div class="form-group">
+                    {!! Form::textarea('images_info[]',$item->info, ['class' => 'form-control view-write', 'placeholder' => trans('re.description'), 'rows' => 7, 'cols' => 40]) !!}
+                    <div class='view-read'>
+                        {!! $item->info !!}
+                    </div>
+                </div>
+            </td>
+            <td>
+                <span href="#" title='view' class='view'><i class="fa fa-eye fa-2x"></i></span>
+                <span href="#" title='edit' class="edit margin-left-5"><i class="fa fa-pencil-square-o fa-2x"></i></span>
+                <span href="#" title='delete' class="margin-left-5"><i class="fa fa-trash-o delete fa-2x"></i></span>
+                <div class="ud">
+                    <span href="#" title='down' class='down'><i class="fa fa-sort-desc fa-2x" aria-hidden="true"></i></span>
+                    <span href="#" title='up' class='up'><i class="fa fa-sort-asc fa-2x" aria-hidden="true"></i></span>
+                </div>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
 @endif
 
 @section('footer_more_scripts')
@@ -99,28 +102,28 @@ if (!empty($realestate)) {
         $('.showfile .filename').show();
         $('.showfile .del').show();
     });
-    
+
     $(".down").click(function () {
         var $parent = $(this).parents(".item_image");
-        $parent.insertAfter($parent.next()); 
+        $parent.insertAfter($parent.next());
     });
 
     $(".up").click(function () {
         var $parent = $(this).parents(".item_image");
-        $parent.insertBefore($parent.prev()); 
+        $parent.insertBefore($parent.prev());
     });
-    
-    $(".edit").click(function(){
-        
+
+    $(".edit").click(function () {
+
         $(this).parents('.item_image').find('.view-read').hide();
         $(this).parents('.item_image').find('.view-write').show();
-        
+
     });
-    $(".view").click(function(){
-        
+    $(".view").click(function () {
+
         $(this).parents('.item_image').find('.view-read').show();
         $(this).parents('.item_image').find('.view-write').hide();
-        
+
     });
 
 </script>
